5efe99754bc74b1cbaecb90774b0f29514ed98e5
David Miller <dmiller2718@gmail.com>
2020-05-31 12:35:01 -0500

    Prepare relase clr.tools.reader-1.3.2

--- README.md
+++ README.md
@@ -14,14 +14,14 @@ Nuget reference:
 
 Leiningen/Clojars reference:
 
-   [org.clojure.clr/tools.reader "1.0.0-beta3"]
+   [org.clojure.clr/tools.reader "1.3.2"]
 
 
 # Copyright and License #
 
 Original Clojure(JVM) code: 
 
-> Copyright © 2013-2015 Nicola Mometto, Rich Hickey & contributors.
+> Copyright © 2013-2018 Nicola Mometto, Rich Hickey & contributors.
 
 
-Distributed under the Eclipse Public License, the same as Clojure.
+Licensed under the EPL. (See the file epl.html.)
--- src/clojure/Properties/AssemblyInfo.cs
+++ src/clojure/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("David Miller")]
 [assembly: AssemblyProduct("clr.tools.namespace")]
-[assembly: AssemblyCopyright("Copyright © David Miller 2015")]
+[assembly: AssemblyCopyright("Copyright © Rich Hickey, David Miller 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.3.2.0")]
+[assembly: AssemblyFileVersion("1.3.2.0")]
