21ea35ca8b9318bd2d3251c1756f25e8857336d4
David Miller <dmiller2718@gmail.com>
2024-02-29 16:21:32 -0500

    Prepare releae 1.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojure.clr/tools.reader "1.3.7"
+(defproject org.clojure.clr/tools.reader "1.4.0"
   :description "Port of clojure.org/tool.reader to ClojureCLR"
   :url "https://github.com/clojure/clr.tools.reader"
   :license {:name "Eclipse Public License"
